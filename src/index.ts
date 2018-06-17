751064d8990acdb909c99a64a2cff98f12df1bf9
Kevin Koshiol <NTARelix@gmail.com>
2018-06-17 11:13:46 -0500

    Remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { debugEmulator } from './debug-emulator'
 import { Emulator } from './emulator'
 import { tick } from './tick'
-import { toHex } from './to-hex'
 
 function startEmulator(cartData: ArrayBuffer, debug: boolean): void {
   const emulator = new Emulator(cartData)
