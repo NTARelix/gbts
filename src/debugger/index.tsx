738a08e35d348bff80aae389d0625d117721b031
Kevin Koshiol <ntarelix@gmail.com>
2023-03-19 20:22:17 -0500

    refactor: migrate to react-dom client api

--- src/debugger/index.tsx
+++ src/debugger/index.tsx
@@ -1,7 +1,8 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Emulator } from '../emulator'
 import { Debugger } from './debugger'
 
 export function renderDebugger(emulator: Emulator, mountNode: HTMLElement): void {
-  render(<Debugger emulator={emulator} />, mountNode)
+  const root = createRoot(mountNode)
+  root.render(<Debugger emulator={emulator} />)
 }
